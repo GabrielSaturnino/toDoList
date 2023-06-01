5ef68b1bc93d5130a9edc1b62173ec4b58565561
Gabriel Saturnino <80297011+GabrielSaturnino@users.noreply.github.com>
2023-06-01 18:48:25 -0300

    tela de tarefas
    parcialmente concluida

---
+++ imports/modules/toDos/ui/pages/style/toDosListStyle.tsx
@@ -0,0 +1,26 @@
+export const toDosStyle = {
+    tabsBox: {
+        width: '100vw',
+        marginTop: '15px',
+        borderBottom: '1px solid gray',
+    },
+    searchField: {
+        marginTop: '66px',
+        border: '1px solid gray',
+        borderRadius: '5px',
+        width: '528px',
+        height: '50px'
+    },
+    buttonFlex: {
+        width: '100%',
+        display: 'flex',
+        justifyContent: 'center'
+    },
+    buttonAddTask: {
+        position: 'fixed',
+        bottom: 50,
+        width: '238px',
+        height: '50px',
+        color: 'black'
+    },
+}
--- imports/modules/toDos/ui/pages/toDosList.tsx
+++ imports/modules/toDos/ui/pages/toDosList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withTracker } from 'meteor/react-meteor-data';
 import { toDosApi } from '../../api/toDosApi';
 import _ from 'lodash';
@@ -9,13 +9,169 @@ import { IToDos } from '../../api/toDosSch';
 import { IConfigList } from '/imports/typings/IFilterProperties';
 import { showLoading } from '/imports/ui/components/Loading/Loading';
 import { AppBar } from '/imports/ui/layouts/MyAppBar';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+
+import InputAdornment from '@mui/material/InputAdornment';
+import TextField from '@mui/material/TextField';
+import SearchIcon from '@mui/icons-material/Search';
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import CloseIcon from '@mui/icons-material/Close';
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+import { toDosStyle } from './style/toDosListStyle';
 
 
 const ToDosList = () => {
+	const [value, setValue] = useState('one');
+	const [searchField, setSearchField] = useState('');
+	const [open, setOpen] = React.useState(false);
+
+	const [taskName, setTaskName] = useState('');
+	const [taskDescription, setTaskDescription] = useState('');
+
+	const [age, setAge] = React.useState('');
+
+	const handleChange = (event) => {
+		setAge(event.target.value);
+	};
+
+	const handleClickOpen = () => {
+		setOpen(true);
+	};
+
+	const handleClose = () => {
+		setOpen(false);
+	};
+
+	const handleChangeValue = (event, newValue: string) => {
+		console.log(taskName);
+		console.log(taskDescription);
+		setValue(newValue);
+	};
+
 	return (
 		<>
 			<AppBar titulo='ToDo List' redirectPage='/meuModulo' />
-			<h1>Olá xD</h1>
+			<Box sx={toDosStyle.tabsBox}>
+				<Container>
+					<Tabs
+						value={value}
+						onChange={handleChangeValue}
+						aria-label="wrapped label tabs example"
+					>
+						<Tab
+							value="one"
+							label="Minhas Tarefas"
+							wrapped
+						/>
+						<Tab value="two" label="Tarefas do Time" />
+					</Tabs>
+				</Container>
+			</Box>
+			<Container>
+
+
+				<TextField
+					sx={toDosStyle.searchField}
+					id="input-with-icon-textfield"
+					value={searchField}
+					onChange={e => setSearchField(e.target.value)}
+					label=""
+					InputProps={{
+						startAdornment: (
+							<InputAdornment position="start">
+								<SearchIcon />
+							</InputAdornment>
+						),
+					}}
+					variant="standard"
+				/>
+				<Box sx={toDosStyle.buttonFlex}>
+					<Button sx={toDosStyle.buttonAddTask} variant={'contained'} color={'primary'}
+						onClick={handleClickOpen}><AddIcon /> Adicionar Tarefa</Button>
+				</Box>
+			</Container>
+
+			<Dialog open={open} onClose={handleClose}>
+				<Box sx={{ width: '727px', height: '856px' }}>
+					<Box sx={{
+						display: 'flex',
+						justifyContent: 'space-between',
+						alignItems: 'center',
+						marginLeft: '20px'
+					}}>
+						<DialogTitle>Adicionar Tarefa</DialogTitle>
+						<CloseIcon sx={{
+							cursor: 'pointer',
+							marginRight: '56px'
+						}}
+							onClick={handleClose} />
+					</Box>
+
+					<Container sx={{
+						width: '80%',
+					}}>
+
+						<DialogContent>
+							<TextField
+								autoFocus
+								margin="dense"
+								id="name"
+								label="Email Address"
+								value={taskName}
+								onChange={e => setTaskName(e.target.value)}
+								type="text"
+								fullWidth
+								variant="filled"
+							/>
+							<TextField
+								id="standard-multiline-static"
+								label="Multiline"
+								value={taskDescription}
+								onChange={e => setTaskDescription(e.target.value)}
+								multiline
+								rows={4}
+								defaultValue="Default Value"
+								variant="standard"
+							/>
+							<FormControl sx={{ m: 1, minWidth: 120 }}>
+								<InputLabel id="demo-simple-select-helper-label">Tipo</InputLabel>
+								<Select
+									labelId="demo-simple-select-helper-label"
+									id="demo-simple-select-helper"
+									value={age}
+									label="Age"
+									onChange={handleChange}
+								>
+									<MenuItem value="">
+									</MenuItem>
+									<MenuItem value={'pessoal'}>Pessoal</MenuItem>
+									<MenuItem value={'publica'}>Publica</MenuItem>
+								</Select>
+							</FormControl>
+						</DialogContent>
+					</Container>
+					<DialogActions>
+						<Box sx={toDosStyle.buttonFlex}>
+							<Button onClick={handleClose}>Salvar</Button>
+						</Box>
+					</DialogActions>
+				</Box>
+			</Dialog>
 		</>
 	);
 };
